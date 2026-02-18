41cc63ab7edde79c0320ccd77d12b5b0e7217aec
Clément POIRET <clement@linux.com>
2026-02-18 19:31:43 +0100

    feat: top level imports

--- src/rollfast/__init__.py
+++ src/rollfast/__init__.py
@@ -1,1 +1,10 @@
+from .optim.prism import prism as prism
+from .optim.psgd import kron as kron
+from .schedules.schedulefree import (
+    schedule_free_eval_params as schedule_free_eval_params,
+    schedule_free_kron as schedule_free_kron,
+    schedule_free_prism as schedule_free_prism,
+)
+from .schedules.wsd import wsd_schedule as wsd_schedule
+
 __version__ = "0.1.0"
