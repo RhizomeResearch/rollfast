f19d116bd5c9babbb2ef1bc415fcf3964b333c25
Clément POIRET <clement@linux.com>
2026-03-11 19:37:59 +0100

    deps: update devenv

--- devenv.nix
+++ devenv.nix
@@ -11,17 +11,13 @@ let
   ];
 in
 {
-  packages = with pkgs; [
-    python312
-  ];
-
   env = {
-    UV_PYTHON = "${pkgs.python312}/bin/python";
     LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
   };
 
   languages.python = {
     enable = true;
+    package = pkgs.python312;
     uv = {
       enable = true;
       sync.enable = true;
